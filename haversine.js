b64b731848d8c5a6f154e71fbff1dc0c05214ef3
Stacy <stacy@meqo.com>
2017-02-12 23:53:16 +0800

    tidied up code based on eslint

--- haversine.js
+++ haversine.js
@@ -1,40 +1,42 @@
-var cos = Math.cos
-var sin = Math.sin
-var sqrt = Math.sqrt
-var PI = Math.PI
+const cos = Math.cos;
+const sin = Math.sin;
+const sqrt = Math.sqrt;
+const PI = Math.PI;
 
 // Earth radius in diff metrics
-var radii = {
+const radii = {
 	km:    6371,
 	mile:  3960,
 	meter: 6371000,
 	nmi:   3440
-}
+};
 
 const haversine = {
 
 	getLatitudeBounds(lat, d, metric) {
 		if (!metric) {
-			metric = 'meter';
+			metric = "meter";
 		}
-		metricPerDeg = (2*PI/360) * radii[metric];
-		degChange = d/metricPerDeg;
+		(arguments.length === 0) && (() => { throw new Error('Please fill in all arguments'); })();		
+		let metricPerDeg = (2*PI/360) * radii[metric];
+		let degChange = d/metricPerDeg;
 		return {
 			lowerLat: lat - degChange,
 			upperLat: lat + degChange
-		}
+		};
 	}, 
 	getLongitudeBounds(lat, long, d, metric) {
 		if (!metric) {
-			metric = 'meter';
+			metric = "meter";
 		}
-		metricPerDeg = (2*PI/360) * radii[metric] * cos(lat * PI/180);
-		degChange = d/metricPerDeg;
+		(arguments.length === 0) && (() => { throw new Error('Please fill in all arguments'); })();
+		let metricPerDeg = (2*PI/360) * radii[metric] * cos(lat * PI/180);
+		let degChange = d/metricPerDeg;
 		return {
 			lowerLong: long - degChange,
 			upperLong: long + degChange
-		}
+		};
 	}	
-}
+};
 
 module.exports = haversine;
